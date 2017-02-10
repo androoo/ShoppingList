7ba3fdff216062c9be48ed9f9eacc2fb9c3c8173
Andrew Gierke <andrew.gierke@gmail.com>
2017-02-10 09:10:21 -0700

    init ShoppingList

---
+++ ShoppingList/Stack.swift
@@ -0,0 +1,14 @@
+//
+//  Stack.swift
+//  ShoppingList
+//
+//  Created by Andrew Ervin Gierke on 2/10/17.
+//  Copyright © 2017 Androoo. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+}
